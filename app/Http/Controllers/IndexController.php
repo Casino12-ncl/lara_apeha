81874c3036a26d626d9bc089726e586784f84fa2
casino12 <istomin_84@mail.ru>
2023-06-08 16:46:58 +0400

    add contacts, mail form

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactFormRequest;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactForm;
+
 
 class IndexController extends Controller
 {
-   public function index(){
+  public function index(){
       $posts = Post::query()->orderBy('created_at', 'DESC')->limit(5)->get();
       
     return view('pivo', [
       'posts' => $posts
     ]);
-   }
+  }
+
+  public function showContactForm()
+  {
+    return view('contact_form');
+  }
+
+  public function contactForm(ContactFormRequest $request)
+  {
+    Mail::to('istomin1984va@yandex.ru')->send(new ContactForm($request->validated()));
+
+    return redirect(route("contacts"));
+  }
 }
